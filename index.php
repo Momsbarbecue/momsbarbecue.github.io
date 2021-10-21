23c5a84c1d4a17f04a98015caffd84afee332bb3
Momsbarbecue <92946929+Momsbarbecue@users.noreply.github.com>
2021-10-21 12:29:32 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php include("templates/header.php"); ?>
+        <!-- Page Header-->
+        <header class="masthead" style="background-image: url('assets/img/ribs.jpeg')">
+            <div class="container position-relative px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-md-10 col-lg-8 col-xl-7">
+                        <div class="site-heading">
+                            <div class ="momshead"><img src="/assets/img/MomsBarbecueWhite.png" alt="alternatetext"></div>
+                            <h1></h1>
+                            <span class="subheading">Only Eve Did More For A Rib</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Main Content-->
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-md-10 col-lg-8 col-xl-7">
+                    <!-- Post preview-->
+                    <div class="post-preview">
+                        <a href="bbqguide.php">
+                            <h2 class="post-title">New to Barbecue and unsure where to start?</h2>
+                            <h3 class="post-subtitle">Use this guide to make your first delicious meal.</h3>
+                        </a>
+                        <p class="post-meta">
+                            Posted by
+                            <a href="#!">Josh Colbert</a>
+                            on October 14, 2021
+                        </p>
+                    </div>
+                    <!-- Divider-->
+                    <hr class="my-4" />
+                    <!-- Post preview-->
+                    <div class="post-preview">
+                        <a href="post.php"><h2 class="post-title">What Kind of Smoker Should You Buy?</h2></a>
+                        <p class="post-meta">
+                            Posted by
+                            <a href="#!">Nate Colbert</a>
+                            on September 18, 2021
+                        </p>
+                    </div>
+                    <!-- Divider-->
+                    <hr class="my-4" />
+                    <!-- Post preview-->
+                    <div class="post-preview">
+                        <a href="post.html">
+                            <h2 class="post-title">Science has not yet mastered prophecy</h2>
+                            <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the next ten.</h3>
+                        </a>
+                        <p class="post-meta">
+                            Posted by
+                            <a href="#!">Start Bootstrap</a>
+                            on August 24, 2021
+                        </p>
+                    </div>
+                    <!-- Divider-->
+                    <hr class="my-4" />
+                    <!-- Post preview-->
+                    <div class="post-preview">
+                        <a href="post.php">
+                            <h2 class="post-title">Failure is not an option</h2>
+                            <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
+                        </a>
+                        <p class="post-meta">
+                            Posted by
+                            <a href="#!">Start Bootstrap</a>
+                            on July 8, 2021
+                        </p>
+                    </div>
+                    <!-- Divider-->
+                    <hr class="my-4" />
+                    <!-- Pager-->
+                    <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer-->
+        <?php include("templates/footer.php"); ?>
